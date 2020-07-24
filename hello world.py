e39001f357d7e687ca9d31d6c3f9d411ac3f94dd
sahulhamid <68674099+sahulhamid@users.noreply.github.com>
2020-07-24 06:26:21 -0700

    Building the Guess word game using random module

--- hello world.py
+++ hello world.py
@@ -1,3 +1,20 @@
+# Building a Guessing word game(hangman) with random module
+import random
+# creating a list of words
 
-print('hello world')
-print('this is sahul hameed')
+list_of_words = ['python', 'swift', 'rudy', 'perl', 'sql', 'html','cprog','kotlin']
+
+# letting the computer to choose word
+word = random.choice(list_of_words)
+
+# Getting dashes in alist format
+guess_word = list('_'*len(word))
+
+# joining the dashes by using the join method
+join_guess_word = ''.join(guess_word)
+
+# player should get 10 turns
+turns = 10
+
+# running a infinite loop(while loop) till 10 turns
+
