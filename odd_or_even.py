7b492bf2009b865a403adc0790bb875d13939b6c
sahulhamid <68674099+sahulhamid@users.noreply.github.com>
2020-07-26 21:59:02 -0700

    opening a function and checking the condition

--- odd_or_even.py
+++ odd_or_even.py
@@ -1,4 +1,11 @@
 # Getting the number form user
 num = int(input('Enterr a number: '))
 
-
+# opening a function
+def even_odd(num):
+    # checking for the even
+    if num % 2 ==0:
+        print(num,'is Even Number')
+    # checking for the odd
+    else:
+        print(num,'is Oddd Number')
