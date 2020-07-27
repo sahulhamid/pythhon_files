dfeb27550443db63a6c0a6b6a5f188b71a1c0573
sahulhamid <68674099+sahulhamid@users.noreply.github.com>
2020-07-26 17:37:13 -0700

    Getting the lenght of the array

--- arrays.py
+++ arrays.py
@@ -1,5 +1,8 @@
 import array
-
 # first we are an empty array
-
 arr = array.array('i',[])
+# getting the length of the array from the user
+arr_len=int(input('Enter the lenght of an array: '))
+
+
+
