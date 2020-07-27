ed89fdd83d60e77d81b05d3e126d7abf0e4cecec
sahulhamid <68674099+sahulhamid@users.noreply.github.com>
2020-07-26 17:40:27 -0700

    Inserting values into array

--- arrays.py
+++ arrays.py
@@ -4,5 +4,11 @@ arr = array.array('i',[])
 # getting the length of the array from the user
 arr_len=int(input('Enter the lenght of an array: '))
 
+for i in range(arr_len):
+    # appendind the values
+    x=int(input('Enter the values: '))
+    arr.append(x)
+
+print(arr)
 
 
