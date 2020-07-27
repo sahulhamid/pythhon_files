6c728df7b6d62528523b20d7f5a55c9efa78f443
sahulhamid <68674099+sahulhamid@users.noreply.github.com>
2020-07-26 21:49:12 -0700

    i have completed my code

--- arrays.py
+++ arrays.py
@@ -5,10 +5,26 @@ arr = array.array('i',[])
 arr_len=int(input('Enter the lenght of an array: '))
 
 for i in range(arr_len):
-    # appendind the values
+    # appending the values
     x=int(input('Enter the values: '))
     arr.append(x)
 
 print(arr)
 
+# asking the user to ener a value
+f = int(input('Enter a value to find: '))
 
+# here i am using while you can use for loop which is much easier than
+
+# searching using  the while loop
+i = 0
+# introducing a constant to find a index of the value
+k = 0
+while i < len(arr):
+    # checking for the number
+    if f == arr[i]:
+        print('Index of the given value is',k)
+    k+=1
+    i+=1
+
+# There is a simple built in function ' index' you can use that
