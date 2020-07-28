6128f29723edb0ad7ab9b8545e788e02da60dc92
sahulhamid <68674099+sahulhamid@users.noreply.github.com>
2020-07-28 15:59:08 -0700

    using a loop

--- find_max.py
+++ find_max.py
@@ -1,4 +1,11 @@
 # Finding maximum number in a list by Sorting
+
 import array
+
 # creating an array
 arr = array.array('i',[])
+
+# asking lengtyh of array
+len_arr = int(input("Enter the legthof array: "))
+
+#using a for loop getting inputs from array
