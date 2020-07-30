da86f8a61250b3e2379a9df7ede41d17add55540
sahulhamid <68674099+sahulhamid@users.noreply.github.com>
2020-07-29 17:51:58 -0700

    Small changes

--- arrays.py
+++ arrays.py
@@ -1,5 +1,5 @@
 import array
-# first we are an empty array
+# first we are creating an empty array
 arr = array.array('i',[])
 # getting the length of the array from the user
 arr_len=int(input('Enter the lenght of an array: '))
--- linear_search.py
+++ linear_search.py
@@ -1,1 +1,18 @@
-print('linear_search')
+# We are going to discuss about Linear Search
+
+#Getting from the user
+import array
+# first we are creating an empty array
+arr = array.array('i',[])
+# getting the length of the array from the user
+arr_len=int(input('Enter the lenght of an array: '))
+
+for i in range(arr_len):
+    # appending the values
+    x=int(input('Enter the values: '))
+    arr.append(x)
+
+# We can have our own list
+lis = [31,28,78,45,69]
+
+
