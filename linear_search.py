f2a9a23cd9f2b156b9d01b6993b8837e65c85a28
sahulhamid <68674099+sahulhamid@users.noreply.github.com>
2020-07-29 18:23:20 -0700

    Completed Linear Search

--- linear_search.py
+++ linear_search.py
@@ -1,4 +1,19 @@
 # We are going to discuss about Linear Search
 
+num_lis = [45,56,78,1,23,56,89]
 
+# Defining a Function
+def linear_search(list):
+    # asking a Number From User
+    num=int(input('Enter the number to find: '))
+    # Running a loop
+    for i in range(len(list)):
+        if num == list[i]:
+            print('Number Found at',i)
+        else:
+            print("Number Not Found")
+
+
+# Calling te function
+linear_search(num_lis)
 
