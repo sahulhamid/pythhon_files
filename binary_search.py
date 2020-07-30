5753381d5a456813a6fa706f7aa800a938a260fa
sahulhamid <68674099+sahulhamid@users.noreply.github.com>
2020-07-29 21:24:47 -0700

    created the function logic

--- binary_search.py
+++ binary_search.py
@@ -14,7 +14,30 @@ while i < len_lis:
     i+=1
 print(lis)
 
+#sorting ths given list
+list = sorted(lis)
+
 # asking the user to enter a number to find
-n = int(input('Enter a Number to Search: '))
+num = int(input('Enter a Number to Search: '))
 
 # Defining the Function of Binary Search
+
+def binary_search(list,num):
+
+    # Lower value
+    # Upper Value
+    l = 0
+    u = len(list) - 1
+    # Iterating a Infinite loop
+    while l <= u:
+        mid = (l+u)//2
+        if list[mid] == num:
+            return True
+        else:
+            if num > list[mid]:
+                l = mid
+            else:
+                u = mid
+
+
+
