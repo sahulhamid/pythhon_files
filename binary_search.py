75ca83feb1d2fe2558b3add290b118da3d0eb48b
sahulhamid <68674099+sahulhamid@users.noreply.github.com>
2020-07-29 21:07:39 -0700

    user created the list

--- binary_search.py
+++ binary_search.py
@@ -1,7 +1,15 @@
 # Getting a list from user
+# You can also take your own list or an array
 #Creating an Empty list
 lis = []
 
 # Asking length of thr length of list
 len_lis = int(input('Enter the Length of List: '))
 
+# Iterating a Loop
+i = 0
+while i < len_lis:
+    num = int(input('Enter values: '))
+    lis.append(num)
+    i+=1
+print(lis)
