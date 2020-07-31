b86d7b93bece8d5b757405169f6df55da55ecd64
sahulhamid <68674099+sahulhamid@users.noreply.github.com>
2020-07-30 20:21:00 -0700

    swapping values

--- Bubble_sort.py
+++ Bubble_sort.py
@@ -6,3 +6,10 @@ lis = [5, 7, 3, 4, 2, 9]
 
 # Defining a Function
 def bubble_sort(num_list):
+    # Iterating a Loop
+    for i in range(len(num_list)-1,0,-1):
+        # Iterating a for single values
+        for j in range(i):
+            if num_list[j] > num_list[j+1]:
+                # Swaping my vales
+
