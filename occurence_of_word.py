3603ed2a4445d965ca7dce34242a527ff53994c4
sahulhamid <68674099+sahulhamid@users.noreply.github.com>
2020-08-07 07:25:03 -0700

    Checking the count of each element in a given list

--- occurence_of_word.py
+++ occurence_of_word.py
@@ -1,3 +1,13 @@
 # Creating a List of words
-words = ['python','java','python','javascript']
+words = ['python','java','python','javascript','java','java']
+
+# Defining a function
+def No_Of_Occur(lis):
+    #Counter
+    counter=[]
+    # Checking the count of each element in the list
+    for i in lis:
+        # storing in a variable
+        c=lis.count(i)
+        counter+=[c]
 
