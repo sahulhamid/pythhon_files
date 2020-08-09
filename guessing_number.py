b641f723d849bb6a0c3d2b637594ba7ed2003910
sahulhamid <68674099+sahulhamid@users.noreply.github.com>
2020-08-09 13:54:02 -0700

    Completed

--- guessing_number.py
+++ guessing_number.py
@@ -10,19 +10,24 @@ print("You have select number from 1 to 15")
 # Getting a Number From User
 num = int(input('Enter a Number: '))
 
-# No of turns that a player can choose a number
+# No Fo turns User can play
 turns = 12
 
 #Checking the input matches or not
 if num == ran_number:
     print("You are Intelligent,Guess is correct")
 else:
+    print('Game Starts')
     # Running an infinite loop
-    while num !=ran_number:
+    while turns >=0 :
         num = int(input('Enter a Number again!: '))
         # checking the the num and ran_num once again
         if num == ran_number:
             print('Great, Guess is correct')
+            turns=0
+        else:
+            print('Remaining Turns are:',turns)
+        turns-=1
 
 
 
