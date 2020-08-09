b6ba299fc57c48d47726c6712d94485476f2e5de
sahulhamid <68674099+sahulhamid@users.noreply.github.com>
2020-08-09 13:38:41 -0700

    no of turns and condition

--- guessing_number.py
+++ guessing_number.py
@@ -1,9 +1,15 @@
 # Importing random module
 import random
-
+#getting a random number from the User
 ran_number = random.randint(1,15)
 print(ran_number)
 
 # Getting a Number From User
 num = int(input('Enter a Number: '))
+
+# No of turns that a player can choose a number
+turns = 12
+
+#Checking the input matches or not
+if num ==
 
