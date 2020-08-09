4fb7bc1f94ed3762b85d05b333cb70db26859240
sahulhamid <68674099+sahulhamid@users.noreply.github.com>
2020-08-09 13:46:05 -0700

    Running an infinite loop

--- guessing_number.py
+++ guessing_number.py
@@ -4,6 +4,9 @@ import random
 ran_number = random.randint(1,15)
 print(ran_number)
 
+# printing the numbers
+print("You have select number from 1 to 15")
+
 # Getting a Number From User
 num = int(input('Enter a Number: '))
 
@@ -12,11 +15,14 @@ turns = 12
 
 #Checking the input matches or not
 if num == ran_number:
-    print("Guess is correct")
+    print("You are Intelligent,Guess is correct")
 else:
     # Running an infinite loop
     while num !=ran_number:
         num = int(input('Enter a Number again!: '))
+        # checking the the num and ran_num once again
+        if num == ran_number:
+            print('Great, Guess is correct')
 
 
 
