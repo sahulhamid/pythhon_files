c276f8482c6aaf467ac4d07a05aca34f26503272
sahulhamid <68674099+sahulhamid@users.noreply.github.com>
2020-08-11 21:52:59 -0700

    Created the Game Board

--- tic_tac-toe.py
+++ tic_tac-toe.py
@@ -1,1 +1,15 @@
-print("Tic Tac Toe")
+# Creating a python game "Tic Tac Toe"
+
+#Creating a board
+board=["-", "-", "-",
+       "-", "-", "-",
+       "-", "-", "-"]
+
+#Displaying the board by definnig a Function
+def dispaly_board():
+    print(board[0]+ "|" + board[1] + "|" + board[2])
+    print(board[3] + "|" + board[4] + "|" + board[5])
+    print(board[6] + "|" + board[7] + "|" + board[8])
+
+
+
