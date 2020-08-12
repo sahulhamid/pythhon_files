78cbd5a7aec65514c1a252bb9e11ffd342f8d989
sahulhamid <68674099+sahulhamid@users.noreply.github.com>
2020-08-11 22:22:36 -0700

    Defining the  Function for the game_over

--- tic_tac-toe.py
+++ tic_tac-toe.py
@@ -1,9 +1,9 @@
 # Creating a python game "Tic Tac Toe"
 
 # Creating a board
-board=["-", "-", "-",
-       "-", "-", "-",
-       "-", "-", "-"]
+board=[" - ",  " - ", " - ",
+       " - ", " - ",  " - ",
+       " - ", " - ", " - "]
 
 # Displaying the board by definig a Function
 def dispaly_board():
@@ -11,19 +11,46 @@ def dispaly_board():
     print(board[3] + "|" + board[4] + "|" + board[5])
     print(board[6] + "|" + board[7] + "|" + board[8])
 
+# Player "X" or "O
+current_palyer=" X "
+
+
+# Boolean Value for the Game flow
+game_going =True
+
+#Handling the game turn
+def handle_game():
+    position = int(input("Enter a Number from 1-9: "))-1
+
+    board[position] = current_palyer
+
+    dispaly_board()
+
+def check_game_over():
+    # Checking for the row
+    check_row()
+    # Checking for the column
+    check_column()
+    # Checking for the diagonals
+    check_diagonal()
+
 
 # Defining a Function for Playing game
 def play_game():
 
-    # Displaying a the Game Board
+    # Displaying a  the Game Board
     dispaly_board()
 
-    # Handling the game turn
-    Handle_turn()
+    while game_going:
+        #Handling the game turn
+        handle_game()
 
-    # Checking the winner or ending the game by tie up the match
-    check_winner()
+        # Checking the winner or ending the game by tie up the match
+        check_game_over()
 
-    # Change the player
-    flip_palyer()
+        # Change the player
+        #flip_palyer()
 
+
+# Calling the Play_Game Function
+play_game()
